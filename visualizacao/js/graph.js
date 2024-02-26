cf6e2d68bd9c6a70c561775c7e4ab4cc860eff80
Karla Falcão <karlapsfalcao@gmail.com>
2024-02-25 23:56:33 -0800

    app v2 with cross filter plugin

---
+++ visualizacao/js/graph.js
@@ -0,0 +1,279 @@
+var margin = {top: 10, left: 10, bottom: 10, right: 20}; 
+var width = 850; // 1000
+var height = 550; // 650
+
+// zoom settings
+var scaleFactor = 0.25;
+var translateFactor = [1200,700];
+var zoomCentered = null;
+var zoomLevel = 0.5;
+
+d3.select("body")
+  .append("h2")
+  .text("Github Network Data Visualization");
+
+d3.select("body").on("contextmenu",d=>{
+  d3.event.preventDefault()
+    return false; });
+
+var svg = d3.select("body")
+  .append("svg")            
+  .attr("width", width)
+  .attr("height", height)
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var border = svg.append("rect")
+  .attr("height", height)
+  .attr("width", width)
+  .attr("x", 0)
+  .attr("y", 0) 
+  .attr("stroke", "grey")
+  .attr("fill", "none") 
+  .attr("stroke-width", "4px");
+
+
+// dataset
+d3.csv("data/edges.csv", function(error, edges){
+  d3.csv("data/gephi/node_data.csv", function(error, csv){
+  	if(error) { console.log(error); }
+		var data_cf = crossfilter(csv);
+    buildPainel();
+		buildGraph(edges, data_cf);
+  });  
+});
+
+
+function buildGraph(edges,data_cf){
+  var links = edges;
+  var nodesConected = {};
+  var nodes = []; 
+
+  var node_data_dm = data_cf.dimension(function(d){ 
+    return d["User"];  }); 
+  var node_data = node_data_dm.bottom(Infinity); 
+
+  var interval = d3.extent(node_data,d=>parseInt(d.In_Degree));
+  var rScale = d3.scaleLinear().domain(interval).range([5, 25]); 
+
+  var qtd = {};
+  node_data.map(e=>{ 
+    return qtd[e.Modularity_Class] = qtd[e.Modularity_Class] + 1 || 1  });
+  var colorScale = d3.scaleSequential(d3.interpolateCool).domain([-3, d3.keys(qtd).length]); // interpolateRainbow interpolateCool
+
+  var initialTransform =  d3.zoomIdentity.scale(1.5).translate(-120,-60);
+
+  links.forEach(function(link) {	
+	  link.source = nodesConected[link.source] || 
+      (nodesConected[link.source] = {User: link.source });			
+	  link.target = nodesConected[link.target] ||
+	    (nodesConected[link.target] = {User: link.target });
+  });
+
+  // var nodes = d3.values(nodesConected); 
+  // ordenando
+  d3.keys(nodesConected)
+    .sort()
+    .forEach(function(v, i) {      
+      nodes[i] = nodesConected[v] 
+  }); 
+
+  // normalizacao
+  node_data.forEach(function(d){   
+    // a principio adotei apenas os repositorios proprios
+    d.Repo_Owner = d.Repo_Owner.split(",");
+    d.oRepo_Language = d.oRepo_Language.split(",");  
+  });
+ 
+  var graph = svg.append("g")
+		.attr("class", "complexNetwork")
+    .attr("transform", "scale(" + scaleFactor + ")translate(" + translateFactor[0] + "," + translateFactor[1] + ")");  
+
+  var link = graph.append("g")
+    .attr("class", "links")
+    .selectAll("links")
+    .data(links)
+    .enter()
+    .append("line")
+    .style("stroke", "#c5a688") 
+    .style("stroke-width", "1px");
+   
+  var node = graph.append("g")
+    .attr("class", "nodes") 
+    .selectAll("nodes")
+    .data(nodes)
+    .enter()
+    .append("g")
+    .attr("cursor", "cell");
+
+  var circle = node.append("circle")
+    .attr("r", (d,i)=>rScale(
+      3*parseInt(node_data[i].In_Degree)  ))
+    .attr("fill", (d,i)=>colorScale(
+      parseInt(node_data[i].Modularity_Class)  ))
+    .attr("stroke", "white")   
+    .attr("stroke-width", "2px");    
+
+  var label = node.append("text")
+    .attr("font-family", "Verdana")
+    .attr("font-size", 10)
+    .attr("text-anchor", "middle") 
+    .text(function(d) { return d.User; }); 
+
+  var forceDirGraph = d3.forceSimulation()
+	  .nodes(nodes)
+    .force("links", d3.forceLink(links));
+     
+  forceDirGraph
+    .force("charge", d3.forceManyBody())           
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    //.force("radial",d3.forceRadial().radius(1.5)) 
+    .force("collision", d3.forceCollide().radius((d,i)=>
+      (3*parseInt(node_data[i].In_Degree) + 5/*normalizacao*/) ));
+
+  // permite a continuacao drag_handler após o fim da simulacao
+  forceDirGraph.on("tick", tick_actions)
+    .on("end", function() {
+      node.each(function(d) {
+    	  d.fx = d.x;
+        d.fy = d.y; })  });
+  
+  var zoom_handler = d3.zoom()
+    .on("zoom", zoom_actions);
+
+  zoom_handler(svg); 
+
+  node.on("click", clicked); 
+
+  var drag_handler = d3.drag()
+    .on("start", drag_start)
+    .on("drag", drag_drag)
+    .on("end", drag_end); 
+
+  drag_handler(node); 
+ 
+  // navegacao por node 
+  // liberacao do node
+  function clicked(d,i) {
+    var x;
+    var y;
+    var level;
+
+    if(!d3.event.ctrlKey){
+      if (zoomCentered !== d) {       
+        x = d.x; 
+        y = d.y; 
+        level = 3*zoomLevel; 
+        zoomCentered = d; 
+
+        zoom_update();
+        graph_update();
+        d3.select(this).select("circle").style("fill", "#74452d");
+        
+        heatmap(node_data[i]);
+        console.log("Dados do usuario:", node_data[i]);
+ 				
+      } else {
+        x = width/2;
+        y = height/2;
+        level = 0.8*zoomLevel;
+        zoomCentered = null;
+
+        links_update();
+        d3.select(this).select("circle").style("fill", colorScale(
+          parseInt(node_data[i].Modularity_Class)  ));
+    	}
+     	graph.transition()
+        .duration(1000)
+        .ease(d3.easeCubicOut)
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + level + ")translate(" + -x + "," + -y + ")");	
+
+    // o usuario libera o nó que fixou    
+    } else {  
+      d.fx = null;
+      d.fy = null;
+
+      links_update();
+      d3.select(this).select("circle").style("fill", colorScale(
+          parseInt(node_data[i].Modularity_Class)  ));
+    }  
+  }
+  
+  function zoom_actions(){
+		var transform = d3.event.transform.scale(scaleFactor).translate(translateFactor[0],translateFactor[1]); 
+		graph.attr("transform", transform);
+		// console.log(transform);
+  } 
+
+  function zoom_update() {  
+  	svg.transition()
+      .duration(1000)
+      .call(zoom_handler.transform, initialTransform);
+	}
+
+  function graph_update() {
+    upNodes = graph.selectAll("circle")
+      .style("fill", (d,i)=>colorScale(
+        parseInt(node_data[i].Modularity_Class)  ));
+    upLink = graph.selectAll("line")
+      .style("stroke", "#c5a688");
+  }
+
+  function links_update() {
+    upLink = graph.selectAll("line")
+      .style("stroke", "#c5a688");
+  }
+
+  function ego_net(user) {
+    var follower = false;
+
+    egoLink = graph.selectAll("line")
+      .style("stroke", d=>{
+        if(user == d.target.User){
+          follower = true;
+          return "#74452d"; // "red"; "#74452d";
+        } else if(user == d.source.User && !follower) {
+            return "#1c4d25"; // "orange" #1c4d25
+        } else {
+            // return "#dbc3aa"; // update enable || disable
+        }  });
+  }
+
+  function drag_start (d) {
+    if (!d3.event.active) 
+      forceDirGraph.alphaTarget(0.5).restart();    
+      d.fx = d.x;
+      d.fy = d.y;
+
+      links_update();      
+      d3.select(this).select("circle").style("fill", "#74452d");
+      ego_net(d.User); 
+  }
+   
+  function drag_drag(d) {
+		d.fx = d3.event.x;
+		d.fy = d3.event.y;
+  }
+
+	// o usuario fixa a posicao do nó
+  function drag_end(d) {
+    if (!d3.event.active) forceDirGraph.alphaTarget(0);
+    	d.fx = d.x;
+      d.fy = d.y;        
+  }
+
+	function tick_actions() {	
+    link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+        
+    circle.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+            
+    label.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  }
+
+// heatmap(node_data[2]);
+
+console.log("node_data:", node_data);    
+}
---
+++ visualizacao/js/interations.js
@@ -0,0 +1,157 @@
+var svgGroup;
+var ht_width = 300;
+var ht_height = 275;
+
+function buildPainel(){
+  var width = 400;
+  // var height = 550;
+
+  // margin.top = 45;
+  // margin.left = 20;
+ 
+  svgGroup = d3.select("body")
+    .append("svg") 
+    .attr("transform", "translate(" + (margin.left) + "," + (margin.top) + ")")           
+    .attr("width", width)
+    .attr("height", height)    
+    .style("background", "#688970");
+   
+  svgGroup
+  	.append("rect") 
+    .attr("transform", "translate(" + margin.left + "," + (margin.top + 10) + ")")
+    .attr("width", ht_width)
+    .attr("height", ht_height + 25)
+    .attr("fill", "#c5a688");
+
+  svgGroup
+    .append("foreignObject")
+    .attr("class", "painel")
+    .attr("transform", "translate(" + margin.left + "," + (margin.top + 10) + ")")
+    .attr("width", ht_width)
+    .attr("height", ht_height + 25)
+    .append("xhtml:div")
+    .html("<h3 style='text-align: center;'>GithubComplex Network of Developers by city" +
+      `
+      </br>
+
+
+
+      <select style='color: #CFDC; text-decoration: underline; text-underline-offset: 5px;background: transparent;
+          font-size: inherit;
+          font-family: cursive;'>
+          <option>
+              
+              <u style="color: #CFDC; text-decoration: underline; text-underline-offset: 5px;">Recife</u>
+          </option>
+      </select>
+      </h3>
+
+
+
+
+      ` + 
+      "<ul style='padding-inline-start: 20px;padding-inline-end: 10px;'>"+
+      "<li>\b Navegue na rede por usuários e veja os seus repositórios próprios e linguagens</li>" +
+      "<li>\b Posicione o nó (usuário) na rede ou libere-o quando quiser (use clique+Ctrl)</li>" +
+      "<li>\b O tamanho dos nós é proporcional </br>a quantidade de seguidores" + 
+      " e as cores representam as comunidades (Louvain Modularity).</li>" +
+      "<ul>"
+    )
+
+  svgGroup.append("image")
+    .attr("class", "painel")
+    .attr("xlink:href", "github_force.png")
+    .attr("transform", "translate(" + margin.left + "," + (ht_height + margin.top)  + ")")
+    // .attr("x", "-14px")
+    // .attr("y", "-8px")
+    .attr("width", ht_width)
+    .attr("height", ht_height);
+}
+
+function heatmap(user){  
+  var ling_repo = {};
+
+  // formatando   
+  user.oRepo_Language.forEach(function(lang,i){   
+    if (ling_repo[lang] === undefined){
+      ling_repo[lang] = [];
+      ling_repo[lang].push(user.Repo_Owner[i]);
+    } else {
+      ling_repo[lang].push(user.Repo_Owner[i]);
+    }
+  });
+  
+  var linguagens = d3.keys(ling_repo).sort();
+  var xScales = {};
+  
+  linguagens.forEach(d=>{
+	  var temp = d3.scaleBand().domain(ling_repo[d]).range([0, ht_width]); // users: mairatma(outline) henvic
+	  xScales[d] = temp;
+  });
+    
+  var yScale = d3.scaleBand().domain(linguagens).range([0, ht_height]);  
+
+  var heatScale = d3.scaleLinear()
+    .domain([1,8,15])
+    .range(['#74452d', 'cyan', '#688970']) 
+    .interpolate(d3.interpolateRgb); 
+
+  var heatMap = svgGroup.append("g")
+  	.attr("class", "heatmap")
+    .attr("width", ht_width);
+    
+
+  // rejoin data 
+  var rows = heatMap.selectAll(".row")
+  	.data(linguagens);  
+
+  // remove unneeded visualizations
+  rows.exit().remove();
+
+  // create new visualizations
+  rows.enter()
+	  .merge(rows)
+	  .append("g")
+	  .attr("class", "row");
+
+  // update selections
+  heatMap
+	  .selectAll(".row")
+	  .data(linguagens)
+	  .selectAll("rect")
+	  .data((x,j)=>ling_repo[x].map(d=>[d,x,j]))
+	  .enter()
+	  .append("rect")
+	  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("width", function(d){return xScales[d[1]].bandwidth(); })
+    .attr("height", function(d){return yScale.bandwidth(); })
+	  .attr("x", function(d){return xScales[d[1]](d[0]); })
+	  .attr("y", function(d){return yScale(d[1]); })	  
+	  .attr("stroke", "white")
+	  .attr("fill", d=>heatScale(d[2]))	  
+    .append("title").text(function(d) { return d[0]; })
+    .on("click", d=>console.log(d));
+
+	var labels = svgGroup.selectAll(".myText")
+  	.data(linguagens);
+
+  labels.exit().remove();
+
+  labelsEnter = labels.enter()  
+    .append("g")
+    .attr("class", "myText");
+
+  labelsEnter.append("text"); 
+
+  labels = labelsEnter.merge(labels);
+
+  labels.select("text")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("x", ht_width+5)
+    .attr("y", function(d,i){
+      return yScale(d)+yScale.bandwidth()*0.55; })
+    .attr("font-family", "Verdana")
+    .attr("font-size", 12)
+    .attr("fill", "white")
+    .text(function(d){return d; }); 
+} 
